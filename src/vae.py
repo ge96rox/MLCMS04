54614454d6203c536b6abea3de71a5df1711b03d
Yinnan Wu <Yinnan.Wu@tum.de>
2021-05-30 12:45:41 +0200

    nedd to fix: show_mnist

--- src/vae.py
+++ src/vae.py
@@ -1,5 +1,10 @@
 import torch
 import torch.nn as nn
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 
 
 class VAE(nn.Module):
@@ -49,3 +54,20 @@ class VAE(nn.Module):
         return 0.5 * (mu.pow(2) + torch.exp((2 * logsigma)) - 2 * logsigma - 1).sum(-1)
 
 
+def show_mnist(nr_row, nr_col, data_loader):
+    fig = plt.figure(figsize=(1.5, 2))
+    nr_total = nr_row * nr_col
+    for i in range(nr_total):
+        idx = np.random.randint(0, len(data_loader))
+        ax = fig.add_subplot(nr_row, nr_col, i + 1)
+        image = data_loader.dataset[idx][0].view(28, 28).numpy()
+        ax.imshow(image)
+
+
+if __name__ == "__main__":
+    batch_size = 128
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST('dataset', train=True, download=True,
+                                   transform=torchvision.transforms.ToTensor(),
+                                   ), batch_size=batch_size, shuffle=True)
+    show_mnist(2, 2, train_loader)
