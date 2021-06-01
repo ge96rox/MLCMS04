80c51b6051a665de77710b4ad3234d7925b58627
Yinnan Wu <Yinnan.Wu@tum.de>
2021-06-01 23:09:50 +0200

    fix vae generate sampling data

--- src/vae.py
+++ src/vae.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 
 
 class VAE(nn.Module):
-    def __init__(self, en_hidden1, en_hidden2, de_hidden1, input_size, output_size):
+    def __init__(self, en_hidden1, latent_space, de_hidden1, input_size, output_size):
         super().__init__()
         self.en_linear1 = nn.Linear(input_size, en_hidden1)
         self.en_relu1 = nn.ReLU()
-        self.en_linear2_mu = nn.Linear(en_hidden1, en_hidden2)
+        self.en_linear2_mu = nn.Linear(en_hidden1, latent_space)
         self.en_relu2_mu = nn.ReLU()
-        self.en_linear2_logsigma = nn.Linear(en_hidden1, en_hidden2)
+        self.en_linear2_logsigma = nn.Linear(en_hidden1, latent_space)
         self.en_relu2_logsigma = nn.ReLU()
-        self.de_linear1 = nn.Linear(en_hidden2, de_hidden1)
+        self.de_linear1 = nn.Linear(latent_space, de_hidden1)
         self.de_relu1 = nn.ReLU()
         self.de_linear2 = nn.Linear(de_hidden1, output_size)
         self.de_relu2 = nn.ReLU()
@@ -55,9 +55,9 @@ class VAE(nn.Module):
     def kl_divergence(self, mu, logsigma):
         return 0.5 * (mu.pow(2) + torch.exp((2 * logsigma)) - 2 * logsigma - 1).sum(-1)
 
-    def gen_sample_data(self, sample_data_size, mu, logsigma):
-        distr = torch.distributions.normal.Normal(mu.mean(0), logsigma.mean(0).exp())
-        z = distr.sample(torch.Size([sample_data_size]))
+    def gen_sample_data(self, sample_data_size, latent_dim):
+        distr = torch.distributions.normal.Normal(0, 1)
+        z = distr.sample(torch.Size([sample_data_size, latent_dim]))
         gendata = self.decoder(z)
         return gendata
 
@@ -74,7 +74,7 @@ def show_mnist(nr_row, nr_col, data_loader):
     plt.show()
 
 
-def show_reconstruct_mnist(nr_row, nr_col, reconstr, y):
+def show_reconstruct_mnist(nr_row, nr_col, reconstr, labels):
     fig = plt.figure(figsize=(10, 6))
     nr_total = nr_row * nr_col
     for i in range(nr_total):
@@ -83,7 +83,7 @@ def show_reconstruct_mnist(nr_row, nr_col, reconstr, y):
         image = reconstr[idx].view(28, 28).detach().numpy()
         ax.imshow(image)
         ax.axis("off")
-        ax.set_title("ground truth {0}".format(y[idx].item()))
+        ax.set_title("ground truth {0}".format(labels[idx].item()))
     plt.show()
 
 
@@ -96,6 +96,11 @@ def show_generate_mnist(nr_row, nr_col, gendata):
         image = gendata[idx].view(28, 28).detach().numpy()
         ax.imshow(image)
         ax.axis("off")
+    plt.show()
+
+def show_latent_distribution(latent_data, labels):
+    latent_z = latent_data.detach().numpy()
+    plt.scatter(latent_z[:, 0], latent_z[:, 1], c=labels, cmap='tab10')
     plt.show()
 
 
