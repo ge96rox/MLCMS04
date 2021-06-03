822cb84b6909b640f308f020225b7e6779776d9d
Qing Sun <670418766@qq.com>
2021-06-03 16:31:22 +0200

    modified model and trained 2-d model

--- src/vae.py
+++ src/vae.py
@@ -7,53 +7,57 @@ import matplotlib.pyplot as plt
 
 
 class VAE(nn.Module):
-    def __init__(self, en_hidden1, latent_space, de_hidden1, input_size, output_size):
+    def __init__(self, en_hidden, latent_space, de_hidden, input_size, output_size):
         super().__init__()
-        self.en_linear1 = nn.Linear(input_size, en_hidden1)
+        self.en_linear1 = nn.Linear(input_size, en_hidden)
         self.en_relu1 = nn.ReLU()
-        self.en_linear2_mu = nn.Linear(en_hidden1, latent_space)
-        self.en_relu2_mu = nn.ReLU()
-        self.en_linear2_logsigma = nn.Linear(en_hidden1, latent_space)
-        self.en_relu2_logsigma = nn.ReLU()
-        self.de_linear1 = nn.Linear(latent_space, de_hidden1)
+        self.en_linear2 = nn.Linear(en_hidden, en_hidden)
+        self.en_relu2 = nn.ReLU()
+        self.en_linear3_mu = nn.Linear(en_hidden, latent_space)
+        self.en_linear3_sigma = nn.Linear(en_hidden, latent_space)
+        self.de_linear1 = nn.Linear(latent_space, de_hidden)
         self.de_relu1 = nn.ReLU()
-        self.de_linear2 = nn.Linear(de_hidden1, output_size)
+        self.de_linear2 = nn.Linear(de_hidden, de_hidden)
         self.de_relu2 = nn.ReLU()
+        self.de_linear3 = nn.Linear(de_hidden, output_size)
+        self.de_relu3 = nn.ReLU()
 
     def forward_elbo(self, x):
-        mu, logsigma = self.encoder(x)
-        z = self.sample_reparameterize(mu, logsigma)
+        mu, sigma = self.encoder(x)
+        z = self.sample_reparameterize(mu, sigma)
         reconstruction = self.decoder(z)
-        return reconstruction, mu, logsigma
+        return reconstruction, mu, sigma
 
-    def loss_function(self, x, reconstruction, mu, logsigma):
+    def loss_function(self, x, reconstruction, mu, sigma):
         mse_loss = nn.MSELoss(reduction='sum')
-        kl = self.kl_divergence(mu, logsigma)
+        kl = self.kl_divergence(mu, sigma)
         outp = -0.5 * mse_loss(x, reconstruction) - kl
         return outp
 
     def encoder(self, x):
         x = self.en_linear1(x)
         x = self.en_relu1(x)
-        mu = self.en_linear2_mu(x)
-        mu = self.en_relu2_mu(mu)
-        logsigma = self.en_linear2_logsigma(x)
-        logsigma = self.en_relu2_logsigma(logsigma)
-        return mu, logsigma
+        x = self.en_linear2(x)
+        x = self.en_relu2(x)
+        mu = self.en_linear3_mu(x)
+        sigma = torch.exp(self.en_linear3_sigma(x))
+        return mu, sigma
 
-    def sample_reparameterize(self, mu, logsigma):
+    def sample_reparameterize(self, mu, sigma):
         epsilon = torch.empty_like(mu).normal_(0., 1.)
-        return epsilon * logsigma.exp() + mu
+        return epsilon * sigma + mu
 
     def decoder(self, z):
         z = self.de_linear1(z)
         z = self.de_relu1(z)
         z = self.de_linear2(z)
         z = self.de_relu2(z)
+        z = self.de_linear3(z)
+        z = self.de_relu3(z)
         return z
 
-    def kl_divergence(self, mu, logsigma):
-        return 0.5 * (mu.pow(2) + torch.exp((2 * logsigma)) - 2 * logsigma - 1).sum(-1)
+    def kl_divergence(self, mu, sigma):
+        return 0.5 * (mu.pow(2) + sigma.pow(2) - 2 * torch.log(sigma) - 1).sum(-1)
 
     def gen_sample_data(self, sample_data_size, latent_dim):
         distr = torch.distributions.normal.Normal(0, 1)
